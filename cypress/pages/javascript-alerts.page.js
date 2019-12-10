14e7949e3e79cff822174adfa0927d9d4fc9dd89
natashawhitter <natasha.whitter@thirdbridge.com>
2019-12-10 17:18:58 +0000

    remove comments added alerts

--- cypress/pages/javascript-alerts.page.js
+++ cypress/pages/javascript-alerts.page.js
@@ -3,34 +3,30 @@ export function visit() {
 }
 
 export function clickAndVerifyJsAlertButton(expectedText) {
-    // const jsAlertButton = cy.contains("Click for JS Alert")
-    // const stub = cy.stub()
-    
-    // jsAlertButton.click()
-    // cy.on('window:alert', stub)
-
-    // expect(stub.getCall(0)).to.be.calledWith("Hi")
-
     const stub = cy.stub()  
-    cy.on ('window:alert', stub)
-    cy.contains("Click for JS Alert").click()
+    cy.on('window:alert', stub)
+    cy.contains('Click for JS Alert').click()
     .then(() => {
       expect(stub.getCall(0)).to.be.calledWith(expectedText)      
     })  
-
 }
 
-export function clickJsConfirmButton() {
-    const jsConfirmButton = cy.contains("Click for JS Confirm")
-    jsConfirmButton.click()
+export function clickAndVerifyJsConfirmButton(expectedText) {
+    const stub = cy.stub()  
+    cy.on('window:confirm', stub)
+    cy.contains('Click for JS Confirm').click()
+    .then(() => {
+      expect(stub.getCall(0)).to.be.calledWith(expectedText)      
+    }) 
 }
 
-export function clickJsPromptButton() {
-    const jsPromptButton = cy.contains("Click for JS Prompt")
-    jsPromptButton.click()
-}
-
-export function checkJsAlert(expectedText) {
+export function clickAndVerifyJsPromptButton() {
+    cy.stub(win, 'prompt').returns('The value you write inside prompt')
+    cy.contains('Click for JS Prompt').click()
+    .then(() => {
+      expect(stub.getCall(0)).to.be.calledWith(expectedText)      
+    }) 
+    
 }
 
 export function checkResult(expectedText) {
